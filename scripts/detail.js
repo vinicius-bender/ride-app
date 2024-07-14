1f6e72007e439d853bb79c888c6fcf30fe04a89f
Vinicius Bender <viniciusbender01@outlook.com>
2024-07-14 19:48:47 -0300

    nao lembro

--- scripts/detail.js
+++ scripts/detail.js
@@ -35,5 +35,13 @@ document.addEventListener("DOMContentLoaded", async () => {
     dataElement.appendChild(dateDiv);
     document.querySelector("#detailsData").appendChild(dataElement);
 
+    const map = L.map("mapDetail");
+    map.setView(firstPosition.latitude, firstPosition.longitude);
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.{ext}', {
+	minZoom: 0,
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+    }).addTo(map);
 });
 
