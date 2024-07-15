f1b640df7f38566f4cbf47f47b4a5df5ad199a85
Vinicius Bender <viniciusbender01@outlook.com>
2024-07-14 21:25:19 -0300

    feito

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,9 @@ allRides.forEach (async ([id, value]) => {
     const firstPosition = ride.data[0];
     const firstLocationData = await getLocationData(firstPosition.latitude, firstPosition.longitude);
 
+    const mapID = `map${ride.id}`;
     const mapElement = document.createElement("div");
+    mapElement.id = mapID;
     mapElement.classList.add("map");
 
 
@@ -49,4 +51,20 @@ allRides.forEach (async ([id, value]) => {
     dataElement.appendChild(dateDiv);
     itemElement.appendChild(mapElement);
     itemElement.appendChild(dataElement);
+
+
+    const map = L.map(mapID, 
+        {zoomControl: false, 
+        dragging: false, 
+        attributionControl: false,
+    scrollWheelZoom: false});
+    map.setView([firstPosition.latitude, firstPosition.longitude], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    minZoom: 5,
+	maxZoom: 20,
+	//attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	ext: 'png'
+    }).addTo(map);
+
+    L.marker([firstPosition.latitude, firstPosition.longitude]).addTo(map);
 });
